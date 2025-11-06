029592e62d1830e8b3767b1e7c2ec15065ebfa06
alexvett <vetokhin.av@phystech.edu>
2025-11-06 23:06:57 +0300

    hw4 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import './App.css';
 import { postsData } from './assets/data.js';
-import PostCard from './components/PostCard.jsx'; 
+import PostCard from './components/PostCard.jsx';
 
 function App() {
-  return (
+   return (
     <div className="app">
       <h1>Лента постов</h1>
-      <div className="posts-container">
+    <div className="posts-container">
         {postsData.map((post) => (
           <PostCard
             key={post.title}
             title={post.title}
             text={post.text}
             currentLikes={post.currentLikes}
+            comments={post.comments}
           />
         ))}
       </div>
     </div>
-  );
+ );
 }
 
 export default App;
--- src/assets/data.js
+++ src/assets/data.js
@@ -2,26 +2,40 @@ export const postsData = [
   {
     "title": "Tesla",
     "text": "Best company",
-    "currentLikes": 7
+    "currentLikes": 7,
+    "comments": [
+      { "id": "c101", "author": "Elon", "text": "Yes!!!!" },
+      { "id": "c102", "author": "User123", "text": "Totally agree" }
+    ]
   },
   {
     "title": "VK",
     "text": "Messenger for children",
-    "currentLikes": 2
+    "currentLikes": 2,
+    "comments": [
+      { "id": "c201", "author": "Pavel", "text": "Ok:)" }
+    ]
   },
   {
     "title": "Me",
     "text": "Best student",
-    "currentLikes": 999
+    "currentLikes": 999,
+    "comments": [
+      { "id": "c301", "author": "Teacher", "text": "Nice!" },
+      { "id": "c302", "author": "Mom", "text": "My best boy!" },
+      { "id": "c303", "author": "Classmate", "text": "Wow" }
+    ]
   },
   {
     "title": "Item",
     "text": "Some other item",
-    "currentLikes": 0
+    "currentLikes": 0,
+    "comments": []
   },
   {
     "title": "Real article",
     "text": "Real text for real article",
-    "currentLikes": 11
+    "currentLikes": 11,
+    "comments": []
   }
 ];
